85eddea36e14445e8db4420065f2f860e781e37d
sharifulalam-dev <sharifulalam.tex@gmail.com>
2024-08-27 20:09:28 +0600

    update last

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: "1rem",
-        screens: {
-          sm: "640px",
-          md: "768px",
-          lg: "1168px", // Custom container width at lg (1024px - 1279px)
-          xl: "1280px", // Default xl size, unchanged
-          "2xl": "1536px", // Default 2xl size, unchanged
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
